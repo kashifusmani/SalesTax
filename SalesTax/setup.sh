b6fa3063bf926c6bf492a7c0d51bf361b045a098
kashifusmani <usmani.kashif9957@gmail.com>
2014-06-26 20:42:36 -0400

    close

--- SalesTax/setup.sh
+++ SalesTax/setup.sh
@@ -6,18 +6,18 @@ export CLASSPATH=$CLASSPATH:$APP_HOME/lib/mockito-all-1.9.5.jar
 echo "setting up binaries"
 rm -rf $APP_HOME/bin/*
 
-javac $APP_HOME/src/com/salestax/businessobjects/Currency.java -d $APP_HOME/lib
-javac $APP_HOME/src/com/salestax/businessobjects/ItemOrigin.java -d $APP_HOME/lib
-javac $APP_HOME/src/com/salestax/businessobjects/ItemType.java -d $APP_HOME/lib
-javac $APP_HOME/src/com/salestax/businessobjects/Price.java -d $APP_HOME/lib
-javac $APP_HOME/src/com/salestax/businessobjects/Item.java -d $APP_HOME/lib
-javac $APP_HOME/src/com/salestax/businessobjects/ItemReceiptEntry.java -d $APP_HOME/lib
-javac $APP_HOME/src/com/salestax/businessobjects/Receipt.java -d $APP_HOME/lib
-javac $APP_HOME/src/com/salestax/util/ValidationHelper.java -d $APP_HOME/lib
-javac $APP_HOME/src/com/salestax/businesslogic/TaxManager.java -d $APP_HOME/lib
-javac $APP_HOME/src/com/salestax/businesslogic/ReceiptCalculator.java -d $APP_HOME/lib
-javac $APP_HOME/src/com/salestax/businesslogic/ReceiptPrinter.java -d $APP_HOME/lib
-javac $APP_HOME/src/com/salestax/businesslogic/ReceiptPrintTest.java -d $APP_HOME/lib
+javac $APP_HOME/src/com/salestax/businessobjects/Currency.java -d $APP_HOME/bin
+javac $APP_HOME/src/com/salestax/businessobjects/ItemOrigin.java -d $APP_HOME/bin
+javac $APP_HOME/src/com/salestax/businessobjects/ItemType.java -d $APP_HOME/bin
+javac $APP_HOME/src/com/salestax/util/ValidationHelper.java -d $APP_HOME/bin
+javac $APP_HOME/src/com/salestax/businessobjects/Price.java -d $APP_HOME/bin
+javac $APP_HOME/src/com/salestax/businessobjects/Item.java -d $APP_HOME/bin
+javac $APP_HOME/src/com/salestax/businessobjects/ItemReceiptEntry.java -d $APP_HOME/bin
+javac $APP_HOME/src/com/salestax/businessobjects/Receipt.java -d $APP_HOME/bin
+javac $APP_HOME/src/com/salestax/businesslogic/TaxManager.java -d $APP_HOME/bin
+javac $APP_HOME/src/com/salestax/businesslogic/ReceiptCalculator.java -d $APP_HOME/bin
+javac $APP_HOME/src/com/salestax/businesslogic/ReceiptPrinter.java -d $APP_HOME/bin
+javac $APP_HOME/src/com/salestax/businesslogic/ReceiptPrintTest.java -d $APP_HOME/bin
 echo "done"
 
-java $APP_HOME/src/com/salestax/businesslogic/ReceiptPrintTest
+java $APP_HOME/bin/com/salestax/businesslogic/ReceiptPrintTest
